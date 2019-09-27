eff2b45192e9dfbd1b1a98af9a0f714b809b8437
Mike Lofgren <mikeylofgren@gmail.com>
2019-09-27 09:53:03 -0700

    add coordinates

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 
 const mapStyles = {
@@ -8,6 +8,32 @@ const mapStyles = {
 };
 
 export class MapContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stores:[
+        {latitutde:45.633, longitude:-120.909},
+        {latitutde:45.700, longitude:-121.403},
+        {latitutde:45.546, longitude:-122.373},
+        {latitutde:45.703, longitude:-121.505},
+        {latitutde:45.710, longitude:-121.362},
+        {latitutde:45.682, longitude:-121.300},
+        {latitutde:45.595, longitude:-121.870},
+      ]
+    }
+
+
+    displayMarkers = () => {
+      return this.state.stores.map((store, index) => {
+        return <Marker key={index} id={index} position={{
+          lat: store.latitutde,
+          lng: store.longitude
+        }}
+      })
+    }
+  }
+  
+  
   render() {
     return (
       <Map
