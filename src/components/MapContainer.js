ddfa931d479f4ef800547e59cab2f6e137788ce7
Mike Lofgren <mikeylofgren@gmail.com>
2019-09-27 11:55:41 -0700

    add info window

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
 
 const mapStyles = {
@@ -11,6 +11,9 @@ export class MapContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
       stores:[
         {latitutde:45.633, longitude:-120.909},
         {latitutde:45.700, longitude:-121.403},
@@ -21,17 +24,38 @@ export class MapContainer extends React.Component {
         {latitutde:45.595, longitude:-121.870},
       ]
     }
+  }
+      
+      onMarkerClick = (props, marker, e) =>
+      //shows info window by passing in marker and place changing info window state to true
+      this.setState({
+        selectedPlace: props,
+        activeMarker: marker,
+        showingInfoWindow: true
+      });
 
+      onClose = props => {
+        // if the infowindow state is equal to true on close this changes it to false and marker is inactive
+        if (this.state.showingInfoWindow) {
+          this.setState({
+            showingInfoWindow: false, 
+            activeMarker: null
+          })
+        }
+      }
 
-    displayMarkers = () => {
+      displayMarkers = () => {
+      //map through array looks at each index
       return this.state.stores.map((store, index) => {
+        //return each position from that ref point
         return <Marker key={index} id={index} position={{
           lat: store.latitutde,
           lng: store.longitude
         }}
+        //add some functionality to prop being passed in
+        onClick = {() => console.log("You clikced me!")} />
       })
     }
-  }
   
   
   render() {
@@ -41,11 +65,28 @@ export class MapContainer extends React.Component {
         zoom={10}
         style={mapStyles}
         initialCenter={{ lat: 45.520, lng: -122.01}}
-      />
+        >
+        {this.displayMarkers()}
+        <Marker
+        onClick={this.onMarkerClick}
+        name={'Some place need to add dynamic stuff here'}
+        />
+        <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}
+        onClose={this.onClose}
+        >
+        <div>
+          <h4>{this.state.selectedPlace.name}</h4>
+        </div>
+        </InfoWindow>
+      </Map>
     );
   }
 }
+  
 
 export default GoogleApiWrapper({
   apiKey: "AIzaSyCi0237UFPbZVMuAUSls22QBuk7XTce9ko"
 })(MapContainer);
+
