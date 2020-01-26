d7e83b84d2258cb7faea6c4d1cafc5dc3e47cebd
Mike Lofgren <mikeylofgren@gmail.com>
2020-01-26 15:32:55 -0800

    deploy to firebase.

---
+++ dist/precache-manifest.70f0cd7e242a6d2b75b7fba56dd5f9af.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16555c5ded489aa7e8ae5735360d9804",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76c96f31862200927a90",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "af55b873540798c38410",
+    "url": "/static/css/main.6a2adc06.chunk.css"
+  },
+  {
+    "revision": "76c96f31862200927a90",
+    "url": "/static/js/2.13a5e773.chunk.js"
+  },
+  {
+    "revision": "58df62b05596037c9a820ae25170d792",
+    "url": "/static/js/2.13a5e773.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af55b873540798c38410",
+    "url": "/static/js/main.77fa27da.chunk.js"
+  },
+  {
+    "revision": "7b724626b6cc4793e605",
+    "url": "/static/js/runtime-main.25f9e2fc.js"
+  },
+  {
+    "revision": "2617eb4384ac3ae93ed8bb5acf7309a9",
+    "url": "/static/media/cloud1.2617eb43.png"
+  },
+  {
+    "revision": "4d7092198e84cc91db1e25979b205998",
+    "url": "/static/media/cloud2.4d709219.png"
+  },
+  {
+    "revision": "afcf0a5e44d20725570d72b2e3f37abb",
+    "url": "/static/media/cloud3.afcf0a5e.jpg"
+  },
+  {
+    "revision": "9e7c7b8db1e741d293d4800586079466",
+    "url": "/static/media/cloud4.9e7c7b8d.jpg"
+  },
+  {
+    "revision": "927c581035dcdfceefcb4aa3b644ff6c",
+    "url": "/static/media/dirt1.927c5810.png"
+  },
+  {
+    "revision": "bea56935fa90aa3db6753b8ffcff3142",
+    "url": "/static/media/dirt2.bea56935.png"
+  },
+  {
+    "revision": "cdd1346b36c5c9daf47dd0d8a19c07a5",
+    "url": "/static/media/dirt3.cdd1346b.png"
+  },
+  {
+    "revision": "d6ea6b9d10804acff0d1c7bf08906eb5",
+    "url": "/static/media/dirt4.d6ea6b9d.png"
+  },
+  {
+    "revision": "d59417418d981e608261f477d2a2a9c0",
+    "url": "/static/media/grass.d5941741.png"
+  },
+  {
+    "revision": "858f8958eb56029bdb4869a601a12ce5",
+    "url": "/static/media/sun.858f8958.png"
+  },
+  {
+    "revision": "f8eac48ccc0a8e7f4232c828d4f80ff2",
+    "url": "/static/media/tickInfo.f8eac48c.jpeg"
+  },
+  {
+    "revision": "a17970efdb032101eb549e937d562133",
+    "url": "/static/media/tree.a17970ef.png"
+  }
+]);
--- src/components/404notFound.js
+++ src/components/404notFound.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const NotFound = () => {
-    return (
-        <h1>Error ya dummy</h1>
-    );
+  return <h1>Error</h1>;
 };
 
 export default NotFound;
